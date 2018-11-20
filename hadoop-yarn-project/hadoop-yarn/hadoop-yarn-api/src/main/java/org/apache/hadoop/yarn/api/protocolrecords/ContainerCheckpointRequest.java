3454ed9a767d082fd4044bb71114613edbcd018d
k4e <x0unnamed@gmail.com>
2018-11-20 18:18:53 +0900

    ContainerCheckpointRequestの定義とPB実装の追加

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/api/protocolrecords/ContainerCheckpointRequest.java
@@ -0,0 +1,39 @@
+package org.apache.hadoop.yarn.api.protocolrecords;
+
+import org.apache.hadoop.classification.InterfaceAudience.Public;
+import org.apache.hadoop.classification.InterfaceStability.Evolving;
+import org.apache.hadoop.classification.InterfaceStability.Unstable;
+import org.apache.hadoop.yarn.api.records.ContainerId;
+import org.apache.hadoop.yarn.util.Records;
+
+@Public
+@Evolving
+public abstract class ContainerCheckpointRequest {
+
+  @Public
+  @Unstable
+  public ContainerCheckpointRequest newInstance(ContainerId containerId,
+      int port) {
+    ContainerCheckpointRequest req =
+        Records.newRecord(ContainerCheckpointRequest.class);
+    req.setContainerId(containerId);
+    req.setPort(port);
+    return req;
+  }
+  
+  @Public
+  @Unstable
+  public abstract ContainerId getContainerId();
+  
+  @Public
+  @Unstable
+  public abstract void setContainerId(ContainerId containerId);
+  
+  @Public
+  @Unstable
+  public abstract int getPort();
+  
+  @Public
+  @Unstable
+  public abstract void setPort(int port);
+}
